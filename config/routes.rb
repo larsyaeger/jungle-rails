6d8a1a9292d2375ad4703be6b8a986c79edab64f
larsyaeger <larsyaeger@gmail.com>
2023-01-30 15:57:13 -0600

    added post login, get login, get logout

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,11 @@ Rails.application.routes.draw do
   
 
   get 'about' => 'about#index'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
   # The priority is based upon order of creation: first created -> highest priority.
